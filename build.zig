7ed6061a9c332f028872e0307f16e8fb3bbc0867
Stenodyon <stenodyon@gmail.com>
2019-08-13 17:35:48 +0200

    added cross-compiling to windows

--- build.zig
+++ build.zig
@@ -1,9 +1,23 @@
 const Builder = @import("std").build.Builder;
+const builtin = @import("builtin");
 
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
+    const windows = b.option(
+        bool,
+        "windows",
+        "create windows build",
+    ) orelse false;
+
     const exe = b.addExecutable("as404", "src/main.zig");
     exe.setBuildMode(mode);
+    if (windows) {
+        exe.setTarget(
+            builtin.Arch.x86_64,
+            builtin.Os.windows,
+            builtin.Abi.gnu,
+        );
+    }
     exe.linkSystemLibrary("c");
     exe.install();
 
